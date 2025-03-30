ebbc628760c29f62b6346a0de865defcd58a6f65
sdefrancesco <skaterbastian@aol.com>
2025-03-30 12:06:54 -0400

    get all countries from REST countries api'

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 
+
+const parent = document.getElementById('countries')
+let countries;
+
+const createCol = (size, parent) => {
+    let col = document.createElement('div')
+    col.classList.add(`col-lg-${size}`)
+}
+
+// get countries
+const getCountries = async() => {
+
+    try {
+        let response = await axios.get('https://restcountries.com/v3.1/all')
+        countries = response.data
+        console.log(countries)
+    } catch(err) {
+        console.log(err)
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    getCountries()
+})
+
+
